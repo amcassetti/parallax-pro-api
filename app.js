e22a4ff3c77aa3ec345f8c233807fd980ea09ea1
Andrew Cassetti <andrew@Andrews-MacBook-Air.local>
2017-03-18 21:17:53 -0400

    new wallpapers

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ var app = express();
 
 var CDN = "https://storage.googleapis.com/parallax-pro-bucket";
 
-var Categories = [{
-	id: 'ci',
-	name: 'Cityscapes',
-	icon: CDN+'/images/ps/thumb/ci1thumb.jpg'
-}];
+var Categories = [
+	{
+		id: 'an',
+		name: 'Animals',
+		icon: CDN+'/images/ps/thumb/an1thumb.jpg'
+	},
+	{
+		id: 'ci',
+		name: 'Cityscape',
+		icon: CDN+'/images/ps/thumb/ci1thumb.jpg'
+	}, 
+	{
+		id: 'la',
+		name: 'Landscape',
+		icon: CDN+'/images/ps/thumb/la1thumb.jpg'
+	}, 
+	{
+		id: 'na',
+		name: 'Nature',
+		icon: CDN+'/images/ps/thumb/na1thumb.jpg'
+	}
+];
 
 var Wallpapers = [
+	// cityscape
 	{
 		id: 'ci1',
 		category: 'ci',
@@ -41,6 +59,80 @@ var Wallpapers = [
 		thumb_url: CDN+'/images/ps/thumb/ci4thumb.jpg',
 		rgba_url: CDN+'/images/ps/rgba/ci4rgba.jpg',
 		depth_url: CDN+'/images/ps/z/ci4z.jpg'
+	},
+
+	// landscape
+	{
+		id: 'la1',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la1thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la1rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la1z.jpg'
+	},
+	{
+		id: 'la2',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la2thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la2rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la2z.jpg'
+	},
+	{
+		id: 'la3',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la3thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la3rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la3z.jpg'
+	},
+	{
+		id: 'la4',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la4thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la4rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la4z.jpg'
+	},
+	{
+		id: 'la5',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la5thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la5rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la5z.jpg'
+	},
+	{
+		id: 'la6',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la6thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la6rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la6z.jpg'
+	},
+	{
+		id: 'la7',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la7thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la7rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la7z.jpg'
+	},
+	{
+		id: 'la8',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la8thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la8rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la8z.jpg'
+	},
+	{
+		id: 'la9',
+		category: 'la',
+		name: '',
+		thumb_url: CDN+'/images/ps/thumb/la9thumb.jpg',
+		rgba_url: CDN+'/images/ps/rgba/la9rgba.jpg',
+		depth_url: CDN+'/images/ps/z/la9z.jpg'
 	}
 ];
 
@@ -53,12 +145,22 @@ app.get('/categories', function(req, res){
 
 });
 
+app.get('/categories/:id', function(req, res){
+
+	let catWalls = Wallpapers.filter(function(c){
+		return c.id = req.params.id
+	});
+
+  	res.status(200);
+  	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ res: catWalls }));
+});
+
 app.get('/wallpapers', function(req, res){
 
   	res.status(200);
   	res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ res: Wallpapers }));
-
 });
 
 var server = app.listen(process.env.PORT || 8081, function(){
